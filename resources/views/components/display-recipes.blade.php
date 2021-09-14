95dc6b92fe604bbc1b6d23a858fbf96983014ced
seb-23 <sebby23@rams.colostate.edu>
2021-09-14 08:17:41 -0600

    display the recipes of all & just of user for specialized actions

---
+++ resources/views/components/display-recipes.blade.php
@@ -0,0 +1,59 @@
+<div class="flex flex-col">
+    <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+            <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                <table class="min-w-full divide-y divide-gray-200">
+                    <thead class="bg-gray-50">
+                    <tr>
+                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                            Recipe
+                        </th>
+
+                        @if (isset($actions))
+                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                Actions
+                            </th>
+                        @endif
+
+                    </tr>
+                    </thead>
+                    <tbody class="bg-white divide-y divide-gray-200">
+
+                    @forelse($recipes as $recipe)
+                        <tr>
+                            <td class="px-6 py-4 whitespace-nowrap">
+                                <div class="flex items-center">
+                                    <div class="flex-shrink-0 h-10 w-10">
+                                        <img class="h-10 w-10 rounded-full" src="{{$recipe->img_url}} ? {{$recipe->img_url}} : {{asset('/images/default-v1.jpeg')}}" alt="">
+                                    </div>
+                                    <div class="ml-4">
+                                        <div class="text-sm font-medium text-gray-900">
+                                            <h1>
+                                                <a href="{{route('recipes.show', $recipe)}}">{{  $recipe->title  }}</a>
+                                            </h1>
+                                        </div>
+                                        <div class="text-sm text-gray-500">
+                                            <a href="/recipes/{{  $recipe->id   }}"> {{  substr($recipe->instructions, 0, 50)  }}... </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </td>
+
+                            @if (isset($actions))
+                                {{$actions}}
+                            @endif
+
+                        </tr>
+
+                    @empty
+                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                            No Relevant Recipes Yet
+                        </td>
+                    @endforelse
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/recipes/user-index.blade.php
@@ -0,0 +1,50 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Recipes') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <x-display-recipes :recipes="$recipes">
+                <x-slot name="actions">
+                    <td class="">
+                        <!-- This example requires Tailwind CSS v2.0+ -->
+                        <div class="relative flex pl-5">
+                            <span class="relative z-0 inline-flex shadow-sm rounded-md -space-x-px">
+                                <a href="">
+                                  <button type="button" class="relative inline-flex items-center px-4 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-400 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500">
+                                    <span class="sr-only">Edit</span>
+                                      <!-- Heroicon name: solid/pencil -->
+                                    <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                      <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
+                                    </svg>
+                                  </button>
+                                </a>
+
+                              <button type="button" class="relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-400 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500">
+                                <span class="sr-only">View</span>
+                                  <!-- Heroicon name: solid/annotation -->
+                                <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                  <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
+                                  <path fill-rule="evenodd" d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z" clip-rule="evenodd" />
+                                </svg>
+                              </button>
+
+                              <button type="button" class="relative inline-flex items-center px-4 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-400 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500">
+                                <span class="sr-only">Delete</span>
+                                  <!-- Heroicon name: solid/trash -->
+                                <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                  <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" />
+                                </svg>
+                              </button>
+                            </span>
+                        </div>
+
+                    </td>
+                </x-slot>
+            </x-display-recipes>
+        </div>
+    </div>
+</x-app-layout>
