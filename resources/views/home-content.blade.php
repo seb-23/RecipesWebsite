70c287fd8dc81dc7601b35d3c8e7121fadc5a7f5
seb-23 <sebby23@rams.colostate.edu>
2021-09-16 14:32:49 -0600

    create home page content

---
+++ resources/views/home-content.blade.php
@@ -0,0 +1,89 @@
+<!-- hero -->
+<div class="w-full py-36 bg-cover bg-no-repeat bg-center relative z-10" style="background-image: url('https://www.lironboylston.com/wp-content/uploads/2020/12/WBC_7095-1536x1024.jpg');">
+
+    <div class="container max-w-4xl mx-auto text-center">
+        <h1 class="text-xl leading-tight md:text-3xl text-center text-gray-100 mb-3">Lorem ipsum dolor sit amet</h1>
+        <p class="text-md md:text-lg text-center text-white ">Ut enim ad minim veniam, quis nostrud exercitation</p>
+
+        <a href="{{route('recipes.create')}}" class="mt-6 inline-block bg-white text-black no-underline px-4 py-3 shadow-lg">Build a recipe</a>
+    </div>
+
+</div>
+<!-- /hero -->
+
+<!-- home content -->
+<div class="w-full px-6 py-12 bg-white">
+    <div class="container max-w-4xl mx-auto text-center pb-10">
+
+        <h3 class="text-xl md:text-3xl leading-tight text-center max-w-md mx-auto text-gray-900 mb-12">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        </h3>
+
+        <a href="{{route('recipes.public')}}" class="bg-black text-white px-4 py-3 no-underline">Browse our recipes</a>
+
+    </div>
+
+    <div class="container max-w-4xl mx-auto text-center flex flex-wrap items-start md:flex-no-wrap">
+
+        <div class="my-4 w-full md:w-1/3 flex flex-col items-center justify-center px-4">
+            <img class="w-full h-64 object-cover mb-6" src="https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F9%2F2019%2F02%2F13%2Ffwcooks-grilled-lobster-tails-with-pasta-recipe2019_344.jpg" />
+
+            <h2 class="text-xl leading-tight mb-2">Ut enim ad minim veniam officia deserunt</h2>
+            <p class="mt-3 mx-auto text-sm leading-normal">Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur.</p>
+        </div>
+
+        <div class="my-4 w-full md:w-1/3 flex flex-col items-center justify-center px-4">
+            <img src="https://littlespicejar.com/wp-content/uploads/2020/07/Lomo-Saltado-Peruvian-Stir-Fry-7-735x1102.jpg" class="w-full h-64 object-cover mb-6" />
+
+            <h2 class="text-xl leading-tight mb-2">Ut enim ad minim veniam officia deserunt</h2>
+            <p class="mt-3 mx-auto text-sm leading-normal">Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur.</p>
+        </div>
+
+        <div class="my-4 w-full md:w-1/3 flex flex-col items-center justify-center px-4">
+            <img src="https://www.simplyrecipes.com/thmb/guUt8eMy7mZn6Aw62nVMWSorQ3U=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2018__07__Seafood-Paella-LEAD-VERTICAL-fc5f1e71baa8484cafa1a106ffaa9c23.jpg" class="w-full h-64 object-cover mb-6" />
+
+            <h2 class="text-xl leading-tight mb-2">Ut enim ad minim veniam officia deserunt</h2>
+            <p class="mt-3 mx-auto text-sm leading-normal">Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur.</p>
+        </div>
+
+    </div>
+</div>
+<!-- /home content -->
+
+<!-- about -->
+<div class="w-full px-6 py-12 text-left bg-gray-100 text-gray-700 leading-normal">
+    <div class="container max-w-4xl mx-auto flex justify-center flex-wrap md:flex-no-wrap">
+        <div class="w-full md:w-1-3">
+            <h3 class="text-3xl mb-8 text-black leading-tight">
+                Lorem ipsum dolor sit amet, consectetur adipisicing.
+            </h3>
+
+            <p class="mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut.</p>
+            <p>Aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        <div class="w-full md:w-2-3 pt-10 md:px-6 md:pt-0">
+            <img src="https://marigoldmaison.com/wp-content/uploads/Indian-Cuisine.jpg" class="w-full h-auto" />
+        </div>
+    </div>
+</div>
+<!-- /about -->
+
+<!-- footer -->
+<footer class="w-full bg-white px-6 border-t">
+    <div class="container mx-auto max-w-4xl py-6 flex flex-wrap md:flex-no-wrap justify-between items-center text-sm">
+        &copy;2021 Sebastian. All rights reserved.
+        <div class="pt-4 md:p-0 text-center md:text-right text-xs">
+            <a href="#" class="text-black no-underline hover:underline">Privacy Policy</a>
+            <a href="#" class="text-black no-underline hover:underline ml-4">Terms &amp; Conditions</a>
+            <a href="#" class="text-black no-underline hover:underline ml-4">Contact Us</a>
+        </div>
+    </div>
+</footer>
+<!-- /footer -->
