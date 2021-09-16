f525f175e5d0140a7034f9dc0a391156e271182c
seb-23 <sebby23@rams.colostate.edu>
2021-09-16 14:42:28 -0600

    refactor code with icons blade file

--- resources/views/recipes/actions.blade.php
+++ resources/views/recipes/actions.blade.php
@@ -1,47 +1,27 @@
-<td>
-    <div class="relative flex pl-5">
-        <span class="relative z-0 inline-flex shadow-sm rounded-md -space-x-px">
-            <a href="{{route('recipes.edit', ['recipe' => $recipe])}}">
-              <button type="button" class="relative inline-flex items-center px-4 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-400 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500">
-                <span class="sr-only">Edit</span>
-                  <!-- Heroicon name: solid/pencil -->
-                <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                  <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
-                </svg>
-              </button>
-            </a>
+<div class="relative flex pl-5">
+    <span class="relative z-0 inline-flex shadow-sm rounded-md -space-x-px">
+        <a href="{{route('recipes.edit', ['recipe' => $recipe])}}">
+          <button type="button" class="rounded-l-md relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-400 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500">
+            <x-icons type="edit" class="h-5 w-5" aria-hidden="true" viewBox="0 0 20 20" fill="currentColor" />
+          </button>
+        </a>
 
-            <a href="{{route('recipes.show', ['recipe' => $recipe])}}">
-              <button type="button" class="relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-400 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500">
-                <span class="sr-only">View</span>
-                  <!-- Heroicon name: solid/annotation -->
-                <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                  <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
-                  <path fill-rule="evenodd" d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z" clip-rule="evenodd" />
-                </svg>
-              </button>
-            </a>
+        <a href="{{route('recipes.show', ['recipe' => $recipe])}}">
+          <button type="button" class="relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-400 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500">
+              <x-icons type="eye" class="h-5 w-5" aria-hidden="true" viewBox="0 0 20 20" fill="currentColor"/>
+          </button>
+        </a>
 
-            <form method="POST" action="{{route('recipes.destroy', ['recipe' => $recipe])}}">
-                @csrf
-                @method('DELETE')
-
-                <div class="field is-grouped">
-                    <div class="control">
-
-                      <button type="submit" class="button is-link relative inline-flex items-center px-4 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-400 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500">
-                        <span class="sr-only">Delete</span>
-                          <!-- Heroicon name: solid/trash -->
-                        <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                          <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" />
-                        </svg>
-                      </button>
-
-                    </div>
+        <form method="POST" action="{{route('recipes.destroy', ['recipe' => $recipe])}}">
+            @csrf
+            @method('DELETE')
+            <div class="field is-grouped">
+                <div class="control">
+                  <button type="submit" class="rounded-r-md button is-link relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-400 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500">
+                    <x-icons type="trash-can" class="h-5 w-5" aria-hidden="true" viewBox="0 0 20 20" fill="currentColor"/>
+                  </button>
                 </div>
-
-            </form>
-
-        </span>
-    </div>
-</td>
+            </div>
+        </form>
+    </span>
+</div>
