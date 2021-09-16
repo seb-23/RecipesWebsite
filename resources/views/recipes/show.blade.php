64373a9ab6da44e96ceca227a75eb813bd5f3b22
seb-23 <sebby23@rams.colostate.edu>
2021-09-16 14:38:14 -0600

    update recipes.show so that actions are availablle when logged in

--- resources/views/recipes/show.blade.php
+++ resources/views/recipes/show.blade.php
@@ -1,17 +1,22 @@
 <x-front-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Recipes') }}
+            {{ __('Details') }}
         </h2>
     </x-slot>
 
-    <div class="py-12">
+    @if(isset($actions))
+        <div class="flex justify-center pt-6">
+            @auth
+                @include('recipes.actions', ['recipe' => $recipe])
+            @endauth
+        </div>
+    @endif
+
+    <div class="py-6">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-
-
-            <div class="py-12">
+            <div>
                 <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <!-- This example requires Tailwind CSS v2.0+ -->
                     <div class="bg-white shadow overflow-hidden sm:rounded-lg">
                         <div class="px-4 py-5 sm:px-6">
                             <h3 class="text-lg leading-6 font-medium text-gray-900">
@@ -55,6 +60,16 @@
                                         {{$recipe->notes}}
                                     </dd>
                                 </div>
+
+                                <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                    <dt class="text-sm font-medium text-gray-500">
+                                        Photo
+                                    </dt>
+                                    <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                                        <img class="sm:rounded-md" src="{{$recipe->img_url ?  : asset('/images/default-v1.jpeg')}}" alt="">
+                                    </dd>
+                                </div>
+
                             </dl>
                         </div>
                     </div>
